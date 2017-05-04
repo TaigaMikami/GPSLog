5359a9fdf8ffca0b39bf785f0bb1323f60736949
TaigaMikami <mikamimikami60@gmail.com>
2017-05-05 00:21:22 +0900

    First Commit

---
+++ Location.swift
@@ -0,0 +1,16 @@
+////
+////  Location.swift
+////  GPSLog
+////
+////  Created by 三上大河 on 2017/05/03.
+////  Copyright © 2017年 Gutty. All rights reserved.
+////
+//
+//import RealmSwift
+//
+//class Location: Object{
+//    dynamic var latitude: Double = 0.0
+//    dynamic var longitude: Double = 0.0
+//    dynamic var createdAt = Date(timeIntervalSince1970: 1)
+//}
+//
